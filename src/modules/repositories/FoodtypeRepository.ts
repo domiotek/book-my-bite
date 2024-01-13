bebc18d078b1f04f4a9efd65c7f518d895419517
Mateusz-Platek <mplatek1705@gmail.com>
2024-01-13 22:49:31 +0100

    Fix

--- src/modules/repositories/FoodtypeRepository.ts
+++ src/modules/repositories/FoodtypeRepository.ts
@@ -1,9 +1,8 @@
 import Foodtype from "../models/Foodtype.js";
 
-
 export default class FoodTypeRepository {
     public async getFoodTypes() {
-        Foodtype
         const foodTypeRecords = await global.app.orm.foodtype.findMany();
         return foodTypeRecords.map(ft => new Foodtype(ft.foodtype_id, ft.name));
+    }
 }
