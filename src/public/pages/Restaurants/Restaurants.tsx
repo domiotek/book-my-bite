c532296d4c87eb270b3dcfb1258b6203d262f1c5
Damian <damianomiotek@outlook.com>
2024-01-13 23:40:15 +0100

    Added missing icons in Restaurant page design

--- src/public/pages/Restaurants/Restaurants.tsx
+++ src/public/pages/Restaurants/Restaurants.tsx
@@ -1,76 +1,68 @@
 import React, { CSSProperties } from 'react';
-import {DateTime} from "luxon";
 
 import classes from "./Restaurants.css";
 import SearchBar from '../../components/SearchBar/SearchBar';
 
+import locationImg from "../../assets/ui/location-orange.svg";
+import foodtypeImg from "../../assets/ui/foodtype-orange.svg";
+
 export default function Restaurants() {
 
-  return (
-	<div className={classes.RestaurantsPage}>
-		<SearchBar />
+	const mockRestaurants = [
+		{
+			id: 1,
+			name: "Tomasza 20 Resto Bar",
+			location: "Kraków, Świętego Tomasza 20",
+			foodtype: "Kuchnia Włoska",
+			imgUrl: "restro_bar.jpg"
+		},
+		{
+			id: 2,
+			name: "Olio | Pizza Napoletana",
+			location: "Kraków, Nadwiślańska 7",
+			foodtype: "Kuchnia Włoska",
+			imgUrl: "olio.jpg"
+		},
+		{
+			id: 3,
+			name: "Tomasza 20 Resto Bar",
+			location: "Kraków, Świętego Tomasza 20",
+			foodtype: "Kuchnia Włoska",
+			imgUrl: "restro_bar.jpg"
+		},
+		{
+			id: 4,
+			name: "Olio | Pizza Napoletana",
+			location: "Kraków, Nadwiślańska 7",
+			foodtype: "Kuchnia Włoska",
+			imgUrl: "olio.jpg"
+		},
+	]
 
-		
+	return (
+		<div className={classes.RestaurantsPage}>
+			<SearchBar />
 
-		<div className={classes.RestaurantsWrapper}>
-			<div className={classes.RestaurantPanel}>
-				<div className={classes.Image} style={{"--image-url": "url(/ilustrations/restro_bar.jpg)"} as CSSProperties}/>
-				<h3>Tomasza 20 Restro Bar</h3>
-				<ul>
-					<li>
-						<img src="ff" alt="" />
-						<span>Kraków, Świętego Tomasza 20</span>
-					</li>
-					<li>
-						<img src="" alt="" />
-						<span>Kuchnia Włoska</span>
-					</li>
-				</ul>
-			</div>
-			<div className={classes.RestaurantPanel}>
-				<div className={classes.Image} style={{"--image-url": "url(/ilustrations/olio.jpg)"} as CSSProperties}/>
-				<h3>Olio | Pizza Napoletana</h3>
-				<ul>
-					<li>
-						<img src="ff" alt="" />
-						<span>Kraków, Nadwiślańska 7</span>
-					</li>
-					<li>
-						<img src="" alt="" />
-						<span>Kuchnia Włoska</span>
-					</li>
-				</ul>
-			</div>
-
-			<div className={classes.RestaurantPanel}>
-				<div className={classes.Image} style={{"--image-url": "url(/ilustrations/restro_bar.jpg)"} as CSSProperties}/>
-				<h3>Tomasza 20 Restro Bar</h3>
-				<ul>
-					<li>
-						<img src="ff" alt="" />
-						<span>Kraków, Świętego Tomasza 20</span>
-					</li>
-					<li>
-						<img src="" alt="" />
-						<span>Kuchnia Włoska</span>
-					</li>
-				</ul>
-			</div>
-			<div className={classes.RestaurantPanel}>
-				<div className={classes.Image} style={{"--image-url": "url(/ilustrations/olio.jpg)"} as CSSProperties}/>
-				<h3>Olio | Pizza Napoletana</h3>
-				<ul>
-					<li>
-						<img src="ff" alt="" />
-						<span>Kraków, Nadwiślańska 7</span>
-					</li>
-					<li>
-						<img src="" alt="" />
-						<span>Kuchnia Włoska</span>
-					</li>
-				</ul>
+			<div className={classes.RestaurantsWrapper}>
+				{
+					mockRestaurants.map(rest=>
+						<div key={rest.id} className={classes.RestaurantPanel}>
+							<div className={classes.Image} style={{"--image-url": `url(/ilustrations/${rest.imgUrl})`} as CSSProperties}/>
+							<h3>{rest.name}</h3>
+							<ul>
+								<li>
+									<img src={locationImg} alt="" />
+									<span>{rest.location}</span>
+								</li>
+								<li>
+									<img src={foodtypeImg} alt="" />
+									<span>{rest.foodtype}</span>
+								</li>
+							</ul>
+						</div>
+					)
+				}
 			</div>
 		</div>
-	</div>
-  )
+	)
 }
